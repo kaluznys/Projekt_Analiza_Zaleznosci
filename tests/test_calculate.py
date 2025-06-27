4f0baa1d63cd5820ec6a4934a67072e37ccc3df9
Szymo <sk468753@students.mimuw.edu.pl>
2025-06-27 09:46:24 +0200

    poprawa testów

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),\
-                                                '..',"..", 'my_project')))
+                                                '..',"..", 'analiza_zaleznosci')))
 
 from src.project_analiza_Szymon.calculate import *
 
